5fbf29338495539e2f92a7f23abbd2c716e33151
ySeungJE <dbstmdwp98@naver.com>
2022-01-28 15:11:36 +0900

    complete

---
+++ x_interval_n.java
@@ -0,0 +1,28 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+class x_interval_n {
+	long[] solution(int x, int n) {
+		long[] array = new long[n];
+		for(int i=0; i<array.length; i++) {
+			array[i]=x*(i+1);
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x,n;
+		x_interval_n xn = new x_interval_n();
+		x=sc.nextInt();
+		n=sc.nextInt();
+		
+		long[] array = xn.solution(x,n);
+		
+		IntStream.range(0, n).forEach(i->System.out.println(array[i]));
+	}
+}
