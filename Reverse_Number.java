51d631dc0ed7630dbfadc1435250eaf59c0ddba9
ySeungJE <dbstmdwp98@naver.com>
2022-02-03 16:48:06 +0900

    remove_main()

--- Reverse_Number.java
+++ Reverse_Number.java
@@ -7,10 +7,4 @@ public class Reverse_Number {
 		StringBuilder sb = new StringBuilder(Long.valueOf(n).toString()).reverse();
 		return Arrays.stream((sb.toString().split(""))).mapToInt(s->Integer.parseInt(s)).toArray();
 	}
-	public static void main(String[] args) {
-		long n = 12345;
-		for(int i : Reverse_Number.solution(n)) {
-			System.out.println(i);
-		}
-	}
 }
